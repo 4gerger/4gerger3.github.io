8ee4250d86a0f32b83771151054f854f4774755f
4gerger <gzhlam@gmail.com>
2019-12-01 20:17:59 +1100

    revised version to address prompts

--- Assets/script.js
+++ Assets/script.js
@@ -1,28 +1,89 @@
 // Setting the variable for password
-const resultEl = document.getElementById('password');
+var resultEl = document.getElementById('password');
 
 // Establishing the variable for the buttons
-const generateEl = document.getElementById('generate');
-const clipboardEl = document.getElementById('copy');
+var generateEl = document.getElementById('generate');
+var clipboardEl = document.getElementById('copy');
 
-// Seeking user inputs to determine password criteria and storing them as variables
-const lengthEl = parseFloat(prompt("How many letters does the password need? (Enter a number between 8-128)"));
-const uppercaseEl = confirm("Does the password need uppercase letters?");
-const lowercaseEl = confirm("Does the password need lowercase letters?");
-const numbersEl = confirm("Does the password need numbers?")
-const symbolsEl = confirm("Does the password need special characters?")
 
-const randomFunction = {
-    lower: getRandomLower,
-    upper: getRandomUpper,
-    numbers: getRandomNumbers,
-    symbols: getRandomSymbols,
-};
+// Asking for customer inputs
+function setting() {
+    var lengthEl = parseFloat(prompt("How many letters does the password need? (Enter a number between 8-128)"));
+    while (lengthEl < 8 || lengthEl > 128 || isNaN(lengthEl)) {
+        lengthEl = parseFloat(prompt("Please enter a number between 8-128!"))
+    };
+    // if conditions to limit user input
+    var uppercaseEl = confirm("Does the password need uppercase letters?");
+    var lowercaseEl = confirm("Does the password need lowercase letters?");
+    var numbersEl = confirm("Does the password need numbers?")
+    var symbolsEl = confirm("Does the password need special characters?")
+    // if user puts all false prompt user that they must pick one to be true
 
-// Copy password to clipboard when "Copy to Clipboard" button is pressed
+    while (lowercaseEl === false && uppercaseEl === false && numbersEl === false && symbolsEl === false) {
+        alert("Please click 'OK' for at least one of the following criteria!");
+        var uppercaseEl = confirm("Does the password need uppercase letters?");
+        var lowercaseEl = confirm("Does the password need lowercase letters?");
+        var numbersEl = confirm("Does the password need numbers?")
+        var symbolsEl = confirm("Does the password need special characters?")
+    };
+
+    var randomFunction = {
+        lower: getRandomLower,
+        upper: getRandomUpper,
+        numbers: getRandomNumbers,
+        symbols: getRandomSymbols,
+    };
+
+    function generatePassword(lower, upper, numbers, symbols, length) {
+        var generatePassword = "";
+        var typesCount = lower + upper + numbers + symbols;
+
+        var typesArr = [{ lower }, { upper }, { numbers }, { symbols }].filter(
+            item => Object.values(item)[0]
+        );
+
+        // When user does not confirm any criteria for password
+        if (typesCount === 0) {
+            return "";
+        }
+
+        // Create a loop
+        for (var i = 0; i < length; i += typesCount) {
+            typesArr.forEach(type => {
+                var funcName = Object.keys(type)[0];
+                console.log('funcName:', funcName);
+                generatePassword += randomFunction[funcName]();
+            });
+        }
+
+        var finalPassword = generatePassword.slice(0, length);
+
+        return finalPassword;
+    }
+
+    var length = lengthEl;
+    var hasLower = (lowercaseEl == true);
+    var hasUpper = (uppercaseEl == true);
+    var hasNumber = (numbersEl == true);
+    var hasSymbol = (symbolsEl == true);
+
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+
+}
+
+// Creating a function to call setting and generatePassword functions
+function randomPassword() {
+    setting();
+    generatePassword();
+}
+
+// addEventListener to run randomPassword function to generate a password
+generateEl.addEventListener("click", randomPassword);
+
+// addEventListener to copy password to clipboard
 clipboardEl.addEventListener('click', () => {
-    const textarea = document.createElement('textarea');
-    const password = resultEl.value;
+    var textarea = document.createElement('textarea');
+    var password = resultEl.value;
 
     if (!password) {
         return;
@@ -36,60 +97,26 @@ clipboardEl.addEventListener('click', () => {
     alert('Password copied to clipboard');
 });
 
-// Generate password when "Generate Password" button is pressed
-generateEl.addEventListener("click", () => {
-    const length = lengthEl;
-    const hasLower = (lowercaseEl == true);
-    const hasUpper = (uppercaseEl == true);
-    const hasNumber = (numbersEl == true);
-    const hasSymbol = (symbolsEl == true);
-
-    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
-});
-
-// Generate password function
-function generatePassword(lower, upper, numbers, symbols, length) {
-    let generatePassword = "";
-    const typesCount = lower + upper + numbers + symbols;
-
-    const typesArr = [{ lower }, { upper }, { numbers }, { symbols }].filter(
-        item => Object.values(item)[0]
-    );
-
-    // When user does not confirm any criteria for password
-    if (typesCount === 0) {
-        return "";
-    }
-
-    // Create a loop
-    for (let i = 0; i < length; i += typesCount) {
-        typesArr.forEach(type => {
-            const funcName = Object.keys(type)[0];
-            console.log('funcName:', funcName);
-            generatePassword += randomFunction[funcName]();
-        });
-    }
-
-    const finalPassword = generatePassword.slice(0, length);
-
-    return finalPassword;
-}
-
 // Generator functions
+// use string or array to assign to the criteria
 // ASCII Code Table https://ascii.cl/
 function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    var lowerString = "abcdefghijklmnopqrstuvwxyz";
+    return lowerString[Math.floor(Math.random() * lowerString.length)];
 }
 
 function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    var upperString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return upperString[Math.floor(Math.random() * upperString.length)];
+
 }
 
 function getRandomNumbers() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+    var numberStrings = "1234567890";
+    return numberStrings[Math.floor(Math.random() * numberStrings.length)];
 }
 
 function getRandomSymbols() {
-    const symbols = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-    return symbols[Math.floor(Math.random() * symbols.length)];
+    var symbolString = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+    return symbolString[Math.floor(Math.random() * symbolString.length)];
 }
